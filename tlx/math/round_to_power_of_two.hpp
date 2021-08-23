a9a8a76c40a734b1f771d6eba482ba7166f1f6b4
Florian Kurpicz <florian.kurpicz@tu-dortmund.de>
2021-08-23 16:26:14 +0200

    Make round_to_power_of_two constexpr

--- tlx/math/round_to_power_of_two.hpp
+++ tlx/math/round_to_power_of_two.hpp
@@ -22,7 +22,7 @@ namespace tlx {
 // round_up_to_power_of_two()
 
 template <typename Integral>
-static inline Integral round_up_to_power_of_two_template(Integral n) {
+static constexpr inline Integral round_up_to_power_of_two_template(Integral n) {
     --n;
     for (size_t k = 1; k != 8 * sizeof(n); k <<= 1) {
         n |= n >> k;
@@ -35,32 +35,33 @@ static inline Integral round_up_to_power_of_two_template(Integral n) {
 // round_up_to_power_of_two()
 
 //! does what it says: round up to next power of two
-static inline int round_up_to_power_of_two(int i) {
+static constexpr inline int round_up_to_power_of_two(int i) {
     return round_up_to_power_of_two_template(i);
 }
 
 //! does what it says: round up to next power of two
-static inline unsigned int round_up_to_power_of_two(unsigned int i) {
+static constexpr inline unsigned int round_up_to_power_of_two(unsigned int i) {
     return round_up_to_power_of_two_template(i);
 }
 
 //! does what it says: round up to next power of two
-static inline long round_up_to_power_of_two(long i) {
+static constexpr inline long round_up_to_power_of_two(long i) {
     return round_up_to_power_of_two_template(i);
 }
 
 //! does what it says: round up to next power of two
-static inline unsigned long round_up_to_power_of_two(unsigned long i) {
+static constexpr inline
+unsigned long round_up_to_power_of_two(unsigned long i) {
     return round_up_to_power_of_two_template(i);
 }
 
 //! does what it says: round up to next power of two
-static inline long long round_up_to_power_of_two(long long i) {
+static constexpr inline long long round_up_to_power_of_two(long long i) {
     return round_up_to_power_of_two_template(i);
 }
 
 //! does what it says: round up to next power of two
-static inline
+static constexpr inline
 unsigned long long round_up_to_power_of_two(unsigned long long i) {
     return round_up_to_power_of_two_template(i);
 }
@@ -69,32 +70,34 @@ unsigned long long round_up_to_power_of_two(unsigned long long i) {
 // round_down_to_power_of_two()
 
 //! does what it says: round down to next power of two
-static inline int round_down_to_power_of_two(int i) {
+static constexpr inline int round_down_to_power_of_two(int i) {
     return round_up_to_power_of_two(i + 1) >> 1;
 }
 
 //! does what it says: round down to next power of two
-static inline unsigned int round_down_to_power_of_two(unsigned int i) {
+static constexpr inline
+unsigned int round_down_to_power_of_two(unsigned int i) {
     return round_up_to_power_of_two(i + 1) >> 1;
 }
 
 //! does what it says: round down to next power of two
-static inline long round_down_to_power_of_two(long i) {
+static constexpr inline long round_down_to_power_of_two(long i) {
     return round_up_to_power_of_two(i + 1) >> 1;
 }
 
 //! does what it says: round down to next power of two
-static inline unsigned long round_down_to_power_of_two(unsigned long i) {
+static constexpr inline
+unsigned long round_down_to_power_of_two(unsigned long i) {
     return round_up_to_power_of_two(i + 1) >> 1;
 }
 
 //! does what it says: round down to next power of two
-static inline long long round_down_to_power_of_two(long long i) {
+static constexpr inline long long round_down_to_power_of_two(long long i) {
     return round_up_to_power_of_two(i + 1) >> 1;
 }
 
 //! does what it says: round down to next power of two
-static inline
+static constexpr inline
 unsigned long long round_down_to_power_of_two(unsigned long long i) {
     return round_up_to_power_of_two(i + 1) >> 1;
 }
